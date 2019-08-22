2226af32a3560777f16d8113feb862af8af4e54f
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-08-22 20:03:36 +0500

    Completes basic JS logic

--- widget/widget.js
+++ widget/widget.js
@@ -1,12 +1,55 @@
-$(document).ready(function () {
-  const element = document.createElement('div');
-  element.setAttribute('class', 'idf-widget-container');
-  document.querySelector('#idf-widget').appendChild(element);
+var idfWidget = {
+  parentElem: null,
+  educationalPartner: '',
+  articlesApi: 'https://www.interaction-design.org/widgets/articles',
 
-  $.ajax({
-    url: 'https://www.interaction-design.org/widgets/articles?ep=usabilitygeek'
-  }).done(function(data){
-    $('.idf-widget-container').html(data);
-  })
+  initialize: function (ep) {
+    $(document).ready(function () {
+      idfWidget.educationalPartner = ep;
+      idfWidget.renderInitialData(ep);
+    });
+  },
 
-});
+  renderInitialData: function (ep, attachElem) {
+    var element = document.createElement('div');
+    var containerClass = 'idf-widget-container';
+    element.setAttribute('class', containerClass);
+    $(attachElem ? attachElem : '#idf-widget').append(element);
+    this.parentElem = $('.' + containerClass);
+
+    $.ajax({
+      url: idfWidget.articlesApi + '?ep=' + ep
+    }).done(function (data) {
+      idfWidget.parentElem.html(data);
+      idfWidget.attachButtonListener();
+    }).fail(function () {
+      idfWidget.parentElem.html('<h2>Unable to load Widget Data</h2>');
+    })
+  },
+
+  attachButtonListener: function () {
+    $('.articlesWidget__loadMore').find('button').click(idfWidget.loadMoreArticles)
+  },
+
+  loadMoreArticles: function () {
+    $.ajax({
+      url: idfWidget.articlesApi + '/load-more/' +
+        idfWidget.getLastArticleSlug() + '?ep=' + idfWidget.educationalPartner
+    }).done(function (data) {
+      idfWidget.parentElem.find('.articlesWidget__articles').append(data);
+    }).fail(function () {
+      idfWidget.parentElem
+        .find('.articlesWidget__articles')
+        .append('<h3>Error: Can not load further data</h3>');
+    })
+  },
+
+  getLastArticleSlug: function () {
+    var lastItem = idfWidget.parentElem.find('li').last().find('a');
+    var originalString = lastItem.attr('href');
+    return originalString.substring(
+      originalString.indexOf('/article/') + 9,
+      originalString.indexOf('?'),
+    );
+  }
+}
