aa90eeddc6c061d2948eb6a2fa5e81330ad53237
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-08-23 10:32:10 +0500

    Adds missing eslint

--- widget/widget.js
+++ widget/widget.js
@@ -1,7 +1,7 @@
 /**
  * Main Widget library file.
- * Can be added to any page to add IDF Article widget 
- * 
+ * Can be added to any page to add IDF Article widget
+ *
  * To use library just initialize with:
  * idfWidget.initialize(educational_partner, element_to_attach_to)
  */
@@ -23,7 +23,7 @@ var idfWidget = {
     var containerClass = 'idf-widget-container';
     element.setAttribute('class', containerClass);
     // use #idf-widget if no other selector passed
-    $(attachElem ? attachElem : '#idf-widget').append(element);
+    $(attachElem || '#idf-widget').append(element);
     idfWidget.parentElem = $('.' + containerClass);
 
     // calls backend for the initial list of articles
@@ -34,19 +34,19 @@ var idfWidget = {
       idfWidget.attachButtonListener(); // Can only attach event after API returns button
     }).fail(function () {
       idfWidget.parentElem.html('<h2>Unable to load Widget Data</h2>');
-    })
+    });
   },
 
   attachButtonListener: function () {
-    $('.articlesWidget__loadMore').find('button').click(idfWidget.loadMoreArticles)
+    $('.articlesWidget__loadMore').find('button').click(idfWidget.loadMoreArticles);
   },
 
   // fired when load more articles button pressed
   loadMoreArticles: function () {
     idfWidget.toggleButtonAvailability(); // disable button
     $.ajax({
-      url: idfWidget.articlesApi + '/load-more/' +
-        idfWidget.getLastArticleSlug() + '?ep=' + idfWidget.educationalPartner
+      url: idfWidget.articlesApi + '/load-more/'
+        + idfWidget.getLastArticleSlug() + '?ep=' + idfWidget.educationalPartner
     }).done(function (data) {
       idfWidget.parentElem.find('.articlesWidget__articles').append(data);
       idfWidget.scrollList(); // refer to function
@@ -56,12 +56,12 @@ var idfWidget = {
       idfWidget.parentElem
         .find('.articlesWidget__articles')
         .append('<h3>Error: Can not load further data</h3>');
-    })
+    });
   },
 
   /**
    * API requires the last loaded article's slug to load the next
-   * list of articles. Easiest approach for getting that is to 
+   * list of articles. Easiest approach for getting that is to
    * locate the last item and get slug present in the href attr
    */
   getLastArticleSlug: function () {
@@ -69,7 +69,7 @@ var idfWidget = {
     var originalString = lastItem.attr('href');
     return originalString.substring(
       originalString.indexOf('/article/') + 9, // 9 is the length of /article/
-      originalString.indexOf('?'), // in all cases the slug ends with ?
+      originalString.indexOf('?') // in all cases the slug ends with ?
     );
   },
 
@@ -86,9 +86,9 @@ var idfWidget = {
   scrollList: function () {
     var list = $('.articlesWidget__articles');
     // we approx the amount to scroll by assuming that each li will be equal to 100px
-    pixelsToScroll = (list.find('li').length * 100).toString();
+    var pixelsToScroll = (list.find('li').length * 100).toString();
     list.animate({
       scrollTop: pixelsToScroll + 'px'
     }, 1000);
   }
-}
+};
