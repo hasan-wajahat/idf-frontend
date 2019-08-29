d64f4935e0d77f7b81d331d2e436002bcae9f4b5
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-08-29 18:00:24 +0500

    Adds initial webpack config

--- src/widget.js
+++ src/widget.js
@@ -6,52 +6,52 @@
  * idfWidget.initialize(educational_partner, element_to_attach_to)
  */
 
-var idfWidget = {
+const idfWidget = {
+
   parentElem: null,
   educationalPartner: '', // IDF's educational partners
   articlesApi: 'https://www.interaction-design.org/widgets/articles',
 
-  initialize: function (ep, attachElem) {
-    $(document).ready(function () {
+  initialize(ep, attachElem) {
+    $(document).ready(() => {
       idfWidget.educationalPartner = ep;
       idfWidget.renderInitialData(ep, attachElem);
     });
   },
 
-  renderInitialData: function (ep, attachElem) {
-    var element = document.createElement('div');
-    var containerClass = 'idf-widget-container';
+  renderInitialData(ep, attachElem) {
+    const element = document.createElement('div');
+    const containerClass = 'idf-widget-container';
     element.setAttribute('class', containerClass);
     // use #idf-widget if no other selector passed
     $(attachElem || '#idf-widget').append(element);
-    idfWidget.parentElem = $('.' + containerClass);
+    idfWidget.parentElem = $(`.${containerClass}`);
 
     // calls backend for the initial list of articles
     $.ajax({
-      url: idfWidget.articlesApi + '?ep=' + ep
-    }).done(function (data) {
+      url: `${idfWidget.articlesApi}?ep=${ep}`,
+    }).done((data) => {
       idfWidget.parentElem.html(data); // API returns template
       idfWidget.attachButtonListener(); // Can only attach event after API returns button
-    }).fail(function () {
+    }).fail(() => {
       idfWidget.parentElem.html('<h2>Unable to load Widget Data</h2>');
     });
   },
 
-  attachButtonListener: function () {
+  attachButtonListener() {
     $('.articlesWidget__loadMore').find('button').click(idfWidget.loadMoreArticles);
   },
 
   // fired when load more articles button pressed
-  loadMoreArticles: function () {
+  loadMoreArticles() {
     idfWidget.toggleButtonAvailability(); // disable button
     $.ajax({
-      url: idfWidget.articlesApi + '/load-more/'
-        + idfWidget.getLastArticleSlug() + '?ep=' + idfWidget.educationalPartner
-    }).done(function (data) {
+      url: `${idfWidget.articlesApi}/load-more/${idfWidget.getLastArticleSlug()}?ep=${idfWidget.educationalPartner}`,
+    }).done((data) => {
       idfWidget.parentElem.find('.articlesWidget__articles').append(data);
       idfWidget.scrollList(); // refer to function
       idfWidget.toggleButtonAvailability(); // enable button
-    }).fail(function () {
+    }).fail(() => {
       idfWidget.toggleButtonAvailability(); // enable button
       idfWidget.parentElem
         .find('.articlesWidget__articles')
@@ -64,17 +64,17 @@ var idfWidget = {
    * list of articles. Easiest approach for getting that is to
    * locate the last item and get slug present in the href attr
    */
-  getLastArticleSlug: function () {
-    var lastItem = idfWidget.parentElem.find('li').last().find('a');
-    var originalString = lastItem.attr('href');
+  getLastArticleSlug() {
+    const lastItem = idfWidget.parentElem.find('li').last().find('a');
+    const originalString = lastItem.attr('href');
     return originalString.substring(
       originalString.indexOf('/article/') + 9, // 9 is the length of /article/
-      originalString.indexOf('?') // in all cases the slug ends with ?
+      originalString.indexOf('?'), // in all cases the slug ends with ?
     );
   },
 
-  toggleButtonAvailability: function () {
-    var button = $('.articlesWidget__loadMore').find('button');
+  toggleButtonAvailability() {
+    const button = $('.articlesWidget__loadMore').find('button');
     button.prop('disabled', !button.prop('disabled'));
   },
 
@@ -83,12 +83,12 @@ var idfWidget = {
    * down automatically so the user knows that articles
    * loaded
    */
-  scrollList: function () {
-    var list = $('.articlesWidget__articles');
+  scrollList() {
+    const list = $('.articlesWidget__articles');
     // we approx the amount to scroll by assuming that each li will be equal to 100px
-    var pixelsToScroll = (list.find('li').length * 100).toString();
+    const pixelsToScroll = (list.find('li').length * 100).toString();
     list.animate({
-      scrollTop: pixelsToScroll + 'px'
+      scrollTop: `${pixelsToScroll}px`,
     }, 1000);
-  }
+  },
 };
